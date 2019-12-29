b955591a47e86d97fa5daa0d6f4aed1364fcd266
QuangDuong120198 <ngo.quang.duong.1100@gmail.com>
2019-12-29 23:45:28 +0700

    Timetable with tsx

---
+++ src/Subject.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export interface SubjectInfoFilter {
+  id?: boolean;
+  name?: boolean;
+  room?: boolean;
+  lecturer?: boolean;
+  note?: boolean;
+}
+
+export interface SubjectProps {
+  id: string;
+  name?: string | null | undefined;
+  room?: string | null | undefined;
+  lecturer?: string | null | undefined;
+  note?: string | null | undefined;
+}
+
+const Subject: React.FC<SubjectProps> = props => {
+  return (
+    <div>
+      <strong>{props.id}</strong>
+      <div>{props.name}</div>
+      <div>{props.room}</div>
+      <div>{props.lecturer}</div>
+      {props.note ?? <em>{props.note}</em>}
+    </div>
+  );
+};
+
+Subject.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  room: PropTypes.string,
+  lecturer: PropTypes.string,
+  note: PropTypes.string
+};
+
+export default Subject;
