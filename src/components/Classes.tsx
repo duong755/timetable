b41df3f5aa1f8220d1b0384b4c42b0498b3c1c9f
duong755 <ngo.quang.duong.1100@gmail.com>
2020-09-05 22:51:58 +0700

    New semester, switch to npm

--- src/components/Classes.tsx
+++ src/components/Classes.tsx
@@ -7,7 +7,7 @@ import useStyles from './styles';
 const Classes: React.FC = () => {
   const classes = useStyles();
   return (
-    <table className="table table-striped table-hover table-bordered">
+    <table className={clsx("table", "table-striped", "table-hover", "table-bordered")}>
       <thead>
         <tr>
           <th></th>
@@ -19,6 +19,100 @@ const Classes: React.FC = () => {
         </tr>
       </thead>
       <tbody>
+        <tr>
+          <td>1</td>
+          <td></td>
+          <td className={classes.subject} rowSpan={3}>
+            <Subject id="INT3507 1" name="Các vấn đề hiện đại của CNTT" room="308 G2" lecturer="PGS. TS. Lê Thanh Hà" />
+          </td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td></td>
+          <td className={classes.subject} rowSpan={3}>
+            <Subject id="ELT2028 4" name="Chuyên nghiệp trong công nghệ" room="413 Viện cơ học" lecturer="TS Nguyễn Hoàng Nam" />
+          </td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>6</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td className={classes.subject} rowSpan={3}>
+            <Subject id="ELT2035 1" room="215 GĐ3" lecturer="TS Lâm Sinh Công" name="Tín hiệu và hệ thống"  />
+          </td>
+          <td></td>
+          <td></td>
+          <td className={classes.subject} rowSpan={4}>
+            <Subject id="INT2212 4" room="303 G2" lecturer="PGS. TS Nguyễn Trí Thành" name="Kiến trúc máy tính" />
+          </td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>8</td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>9</td>
+          <td></td>
+          <td className={classes.subject} rowSpan={4}>
+            <Subject id="POL1001 1" name="Tư tưởng Hồ Chí Minh" room="101 G2" lecturer="ThS Hoàng Thị Ngọc Minh" />
+          </td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>10</td>
+          <td></td>
+          <td className={classes.subject} rowSpan={3}>
+            <Subject id="INT3206 1" name="Cơ sở dữ liệu phân tán" room="308 G2" lecturer="ThS Vũ Bá Duy" />
+          </td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>11</td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>12</td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
       </tbody>
     </table>
   );
